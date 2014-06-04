1624576ba8dae830cdd9eb7c38d8f2f6aafe6855
Taleb <aladin.taleb@gmail.com>
2014-06-04 12:42:09 +0200

    Update for Modular Game

--- action.pl
+++ action.pl
@@ -16,44 +16,46 @@
 %dealSeeds([Boards], PlayerTurn, ChoosedAction, &[NewBoards], &LastField)
 %-------
 	dealSeeds([Board1,Board2], 0, ChoosedAction, [NewBoard1,NewBoard2], LastField) :-
+        nbFields(NbFields),
 		computeFirstIndex(ChoosedAction, FirstIndex),
 		dealBoardActive(Board1, FirstIndex, TotalDealt, NewBoard1),
-		FirstIndex2 is (FirstIndex+6) mod 12,
+		FirstIndex2 is (FirstIndex+NbFields) mod (2*NbFields),
 		dealBoardPassive(Board2, FirstIndex2, TotalDealt, NewBoard2),
 		computeLastField(0, ChoosedAction, TotalDealt, LastField), !.
 		
 	dealSeeds([Board1,Board2], 1, ChoosedAction, [NewBoard1,NewBoard2], LastField) :-
+        nbFields(NbFields),
 		computeFirstIndex(ChoosedAction, FirstIndex),
 		dealBoardActive(Board2, FirstIndex, TotalDealt, NewBoard2),
-		FirstIndex2 is (FirstIndex+6) mod 12,
+		FirstIndex2 is (FirstIndex+NbFields) mod (2*NbFields),
 		dealBoardPassive(Board1, FirstIndex2, TotalDealt, NewBoard1),
 		computeLastField(1, ChoosedAction, TotalDealt, LastField), !.
 
 %-------
 %computeFirstIndex(ChoosedAction, &FirstIndex)
 %-------
-	computeFirstIndex(ChoosedAction, FirstIndex)	:- FirstIndex is (13 - ChoosedAction) mod 12.
+	computeFirstIndex(ChoosedAction, FirstIndex)	:- nbFields(NbFields), FirstIndex is (2*NbFields+1 - ChoosedAction) mod (2*NbFields).
 
 %-------
 %dealBoardActive(Board, FirstIndex, &TotalDealt, &NewBoard)
 %-------
 	dealBoardActive([], _, _, []).
 	dealBoardActive([T|Q], 0, T, [0 | SubBoard])				:- dealBoardActive(Q, 1, T, SubBoard).
-	dealBoardActive([T|Q], Index, TotalDealt, [C | SubBoard])	:- NextIndex is (Index+1) mod 12, dealBoardActive(Q, NextIndex, TotalDealt, SubBoard), C is T + ((TotalDealt-Index) div 11) + 1.
+	dealBoardActive([T|Q], Index, TotalDealt, [C | SubBoard])	:- nbFields(NbFields),NextIndex is (Index+1) mod (2*NbFields), dealBoardActive(Q, NextIndex, TotalDealt, SubBoard), C is T + ((TotalDealt-Index) div (2*NbFields-1)) + 1.
 
 %-------
 %dealBoardPassive(Board, FirstIndex, TotalDealt, &NewBoard)
 %-------
 	dealBoardPassive([], _, _, []).
-	dealBoardPassive(Board, Index, TotalDealt, NewBoard)		:- AddedSeeds is ((TotalDealt-Index) div 11) + 1, subDealBoardPassive(Board, Index, TotalDealt, NewBoard, AddedSeeds).
+	dealBoardPassive(Board, Index, TotalDealt, NewBoard)		:- nbFields(NbFields),AddedSeeds is ((TotalDealt-Index) div (2*NbFields-1)) + 1, subDealBoardPassive(Board, Index, TotalDealt, NewBoard, AddedSeeds).
 
 	subDealBoardPassive(SubB, Index, TotalDealt, SubB, AddedSeeds)				:- AddedSeeds =:= 0, !.
-	subDealBoardPassive([T|Q], Index, TotalDealt, [C|SubBoard], AddedSeeds)		:- C is T+AddedSeeds, NextIndex is (Index+1) mod 12, dealBoardPassive(Q, NextIndex, TotalDealt, SubBoard).
+	subDealBoardPassive([T|Q], Index, TotalDealt, [C|SubBoard], AddedSeeds)		:- nbFields(NbFields),C is T+AddedSeeds, NextIndex is (Index+1) mod (2*NbFields), dealBoardPassive(Q, NextIndex, TotalDealt, SubBoard).
 	
 %-------
 %computeLastField(PlayerTurn, ChoosedAction, TotalDealt, &LastField)
 %-------
-	computeLastField(PlayerTurn, ChoosedAction, TotalDealt, LastField) :- LastField is ((PlayerTurn*6 + ChoosedAction + TotalDealt + TotalDealt div 12 - 1) mod 12) + 1.
+	computeLastField(PlayerTurn, ChoosedAction, TotalDealt, LastField) :- nbFields(NbFields), LastField is ((PlayerTurn*NbFields + ChoosedAction + TotalDealt + TotalDealt div (2*NbFields) - 1) mod (2*NbFields)) + 1.
 
 %******************%
 %*	   Harvest	  *%
@@ -67,7 +69,8 @@
         PlayerTurn \= EnemyPlayer,
         getEnemyBoard([_, Boards, PlayerTurn], EnemyBoard),
         getPlayerScore([Scores, _, PlayerTurn], Score),
-        RelativeLastField is ((LastField-1) mod 6) + 1,
+        nbFields(NbFields),
+        RelativeLastField is ((LastField-1) mod (NbFields div 2)) + 1,
         harvestBoard(EnemyBoard, RelativeLastField, NewEnemyBoard, NewScore),
         enemyBoardIsNotEmpty(NewEnemyBoard), PlayerIndex is PlayerTurn +1,
         NewPlayerScore is Score+NewScore,
